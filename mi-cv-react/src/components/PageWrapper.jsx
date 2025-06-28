ed92f487934279060b0b1f9e94bcb1c9fe47a1fb
Milagros <152735884+itoito0350@users.noreply.github.com>
2025-06-28 23:35:24 +0200

    Update PageWrapper.jsx

--- mi-cv-react/src/components/PageWrapper.jsx
+++ mi-cv-react/src/components/PageWrapper.jsx
@@ -1,38 +1,38 @@
-
 import React, { useEffect, useState } from 'react';
-import {Overlay} from "../styles/PageTransitionStyles";
+import { Overlay } from "../styles/PageTransitionStyles";
 import { AnimatePresence } from 'framer-motion';
-import GlitchBackground from './GlitchBackground'; 
-
+import GlitchBackground from './GlitchBackground';
 
 const PageWrapper = ({ children, isAnimating, setIsAnimating }) => {
   const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
-    setIsAnimating(true);
-    setShowContent(false);
+    const startAnimation = setTimeout(() => {
+      setIsAnimating(true);
+      setShowContent(false);
 
-    const timeout = setTimeout(() => {
-      setIsAnimating(false);
-      setShowContent(true);
-    }, 1000); 
+      const endAnimation = setTimeout(() => {
+        setIsAnimating(false);
+        setShowContent(true);
+      }, 1000); // duración de la animación
 
-    return () => clearTimeout(timeout);
-  }, []);
+      return () => clearTimeout(endAnimation);
+    }, 100); // pequeño delay para evitar saltos iniciales
+
+    return () => clearTimeout(startAnimation);
+  }, [setIsAnimating]);
 
   return (
     <>
-     
       <GlitchBackground isAnimating={isAnimating} />
-      
-      
+
       <AnimatePresence mode="wait">
         {isAnimating && (
           <Overlay
             key="page-transition"
-            initial={{ height: '0%', top: '50%', bottom: '50%' }} 
+            initial={{ height: '0%', top: '50%', bottom: '50%' }}
             animate={{ height: '100%', top: '0%', bottom: '0%' }}
-            exit={{ height: '0%', top: '50%', bottom: '50%' }} 
+            exit={{ height: '0%', top: '50%', bottom: '50%' }}
             transition={{ duration: 1.5, ease: 'easeInOut' }}
           />
         )}
