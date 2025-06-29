f90cd51fcb9b26fad581b3ca8476f3ee9dce45ef
Milagros <152735884+itoito0350@users.noreply.github.com>
2025-06-29 15:26:31 +0200

    Update PageTransition.jsx

--- mi-cv-react/src/components/PageTransition.jsx
+++ mi-cv-react/src/components/PageTransition.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { TransitionContainer, TopLayer, BottomLayer, GlitchLine } from "../styles/PageTransitionStyles";
+import {
+  TransitionContainer,
+  TopLayer,
+  BottomLayer,
+  GlitchLine,
+} from '../styles/PageTransitionStyles';
 
 const PageTransition = ({ isAnimating }) => {
   const [lines, setLines] = useState([]);
@@ -9,13 +14,19 @@ const PageTransition = ({ isAnimating }) => {
       const randomDirection = Math.random() > 0.5 ? 'horizontal' : 'vertical';
       const randomX = Math.random() * window.innerWidth;
       const randomY = Math.random() * window.innerHeight;
-      const minLength = randomDirection === 'horizontal' ? window.innerWidth * 0.5 : window.innerHeight * 0.5;
-      const maxLength = randomDirection === 'horizontal' ? window.innerWidth * 1 : window.innerHeight * 1.5;
+      const minLength =
+        randomDirection === 'horizontal'
+          ? window.innerWidth * 0.5
+          : window.innerHeight * 0.5;
+      const maxLength =
+        randomDirection === 'horizontal'
+          ? window.innerWidth
+          : window.innerHeight * 1.5;
       const randomLength = Math.random() * (maxLength - minLength) + minLength;
       const randomOrigin = Math.random();
 
-      setLines((prevLines) => [
-        ...prevLines,
+      setLines((prev) => [
+        ...prev,
         { randomDirection, randomX, randomY, randomLength, randomOrigin },
       ]);
     };
@@ -28,28 +39,34 @@ const PageTransition = ({ isAnimating }) => {
   return (
     <TransitionContainer>
       <TopLayer
-        initial={{ y: '-100%' }}
+        initial={false}
         animate={{ y: isAnimating ? '0%' : '-100%' }}
-        exit={{ y: '-100%' }}
         transition={{ duration: 0.8, ease: 'easeInOut' }}
       />
       <BottomLayer
-        initial={{ y: '100%' }}
+        initial={false}
         animate={{ y: isAnimating ? '0%' : '100%' }}
-        exit={{ y: '100%' }}
         transition={{ duration: 0.8, ease: 'easeInOut' }}
       />
 
-      {lines.map((line, index) => (
+      {lines.map((line, i) => (
         <GlitchLine
-          key={index}
+          key={i}
           className={line.randomDirection}
           animate={{
             opacity: [0, 0.4, 1, 0.4, 0],
-            width: line.randomDirection === 'horizontal' ? 
-              (line.randomOrigin > 0.5 ? ['0%', '100%'] : ['100%', '0%']) : undefined,
-            height: line.randomDirection === 'vertical' ? 
-              (line.randomOrigin > 0.5 ? ['0%', '100%'] : ['100%', '0%']) : undefined,
+            width:
+              line.randomDirection === 'horizontal'
+                ? line.randomOrigin > 0.5
+                  ? ['0%', '100%']
+                  : ['100%', '0%']
+                : undefined,
+            height:
+              line.randomDirection === 'vertical'
+                ? line.randomOrigin > 0.5
+                  ? ['0%', '100%']
+                  : ['100%', '0%']
+                : undefined,
           }}
           transition={{
             duration: 3 + Math.random(),
@@ -61,8 +78,14 @@ const PageTransition = ({ isAnimating }) => {
           style={{
             left: line.randomDirection === 'vertical' ? `${line.randomX}px` : 0,
             top: line.randomDirection === 'horizontal' ? `${line.randomY}px` : 0,
-            width: line.randomDirection === 'horizontal' ? `${line.randomLength}px` : '1px',
-            height: line.randomDirection === 'vertical' ? `${line.randomLength}px` : '1px',
+            width:
+              line.randomDirection === 'horizontal'
+                ? `${line.randomLength}px`
+                : '1px',
+            height:
+              line.randomDirection === 'vertical'
+                ? `${line.randomLength}px`
+                : '1px',
           }}
         />
       ))}
